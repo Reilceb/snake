231b081b9d3830d919c4639c429910ad0eed4f22
Reilceb <reilceb@gmail.com>
2018-07-19 15:17:50 +0200

    Création

---
+++ test.js
@@ -0,0 +1,12 @@
+let allowedDirections = ["left", "right"];
+
+let x = allowedDirections.indexOf("righ");
+
+console.log(x);
+
+
+var i = 62;
+for (var  i = 0; i < 10; i++) {
+   console.log(i);
+}
+// console.log(i);
